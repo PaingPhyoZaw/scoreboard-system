292c0297ddfe6f6b21961c982ad96b6b35cbb810
paingphyozaw <paingphyozaw54@gmail.com>
2025-04-30 13:24:01 +0630

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000']
+    }
+  },
+  serverExternalPackages: ['@prisma/client', '@supabase/auth-helpers-nextjs'],
   images: {
     unoptimized: true,
   },
+  output: 'standalone',
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+      layers: true
+    }
+    return config
+  }
 }
 
 export default nextConfig
